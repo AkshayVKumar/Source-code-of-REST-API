e012336bb32c28a7ee1ed27840a33b59cf15ea01
unknown <akshayvk64@gmail.com>
2020-04-29 13:38:58 +0530

    created admin and given access for user profiles

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
+from django.contrib.admin import ModelAdmin
 # Register your models here.
+class AdminUserProfile(ModelAdmin):
+    list_display=('email','name','is_active','is_staff','is_superuser')
+admin.site.register(UserProfile,AdminUserProfile)
